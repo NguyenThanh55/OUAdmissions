55d434761e262a07f4788a6648061b7e7a82dfd9
NguyenThanh55 <2051052120thanh@ou.edu.vn>
2023-07-19 22:42:55 +0700

    First

---
+++ OUAdmission/src/main/java/com/myproject/controllers/IndexController.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.myproject.controllers;
+
+import com.myproject.service.CategoryService;
+import com.myproject.service.ProductService;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ *
+ * @author Thanh
+ */
+@Controller
+@PropertySource("classpath:configs.properties")
+public class IndexController {
+    @Autowired
+    private ProductService productService;
+    @Autowired
+    private CategoryService cateService;
+    @Autowired
+    private Environment env;
+    
+    @RequestMapping("/")
+    public String index(Model model, @RequestParam Map<String, String> params) {
+        model.addAttribute("products", this.productService.getProducts(params));
+        model.addAttribute("categories", this.cateService.getCategories());
+        
+        int pageSize = Integer.parseInt(this.env.getProperty("PAGE_SIZE"));
+        long count = this.productService.countProduct();
+        model.addAttribute("counter", Math.ceil(count*1.0/pageSize));
+        
+        return "index";
+    }
+}
---
+++ OUAdmission/src/main/java/com/myproject/service/impl/ProductServiceImpl.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.myproject.service.impl;
+
+import com.myproject.pojo.Product;
+import com.myproject.repository.ProductRepository;
+import com.myproject.service.ProductService;
+import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author admin
+ */
+@Service
+public class ProductServiceImpl implements ProductService {
+    @Autowired
+    private ProductRepository productRepo;
+
+    @Override
+    public List<Product> getProducts(Map<String, String> params) {
+        return this.productRepo.getProducts(params);
+    }
+
+    @Override
+    public Long countProduct() {
+        return this.productRepo.countProduct();
+    }
+    
+}
